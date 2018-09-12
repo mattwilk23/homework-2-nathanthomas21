cc6a102c2a3476796e08436d3b486d7b4eeacd74
Matt Wilkinson <mattwilk23@ksu.edu>
2018-09-11 20:37:55 -0500

    Added Kelvin temperature after Celsius temperature

--- temperature.sh
+++ temperature.sh
@@ -1,8 +1,9 @@
-#! /bin/bash
+./#! /bin/bash
 
 echo -e "Enter a temperature in degrees Farenheit: "
 read temp
 
 echo "scale=3; ($temp - 32.0) * 5.0 / 9.0" | bc
+echo "scale=3; (($temp - 32.0) * 5.0 / 9.0)+273" | bc
 
 #echo "The converted temperature in degrees Celsius is: $CELSIUS"
